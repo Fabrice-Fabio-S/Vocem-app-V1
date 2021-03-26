7fcf3dfca3a7e18a0072ffcb5899b255627658b9
Fabrice-Fabio-S <74406376+Fabrice-Fabio-S@users.noreply.github.com>
2021-03-26 17:02:56 +0100

    add voice-app

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {StyleSheet, Text, View, Button, AppRegistry} from 'react-native';
 import Voice from 'react-native-voice';
-export default class VoiceNative extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-           recognized: '',
-           started: '',
-           results: [],
-       };
-            
-        Voice.onSpeechStart = this.onSpeechStart.bind(this)
-        Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this)
-        Voice.onSpeechResults = this.onSpeechResults.bind(this) 
-    }
+
+class App extends Component {
+  state = {
+    count: 0
+  }
+  constructor(props) {
+    super(props)
+    this.state = {
+       recognized: '',
+       started: '',
+       results: [],
+   };
+        
+    Voice.onSpeechStart = this.onSpeechStart.bind(this)
+    Voice.onSpeechRecognized = this.onSpeechRecognized.bind(this)
+    Voice.onSpeechResults = this.onSpeechResults.bind(this) 
+  }
+
   onSpeechStart(e) {
     this.setState({
       started: '√',
@@ -29,5 +34,70 @@ export default class VoiceNative extends React.Component {
       results: e.value,
     });
   }
+
+  onStartButtonPress(e){
+    Voice.start('en-US');
+  }
+
+  onPress = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  
+  async _startRecognition(e) {
+    this.setState({
+      recognized: '',
+      started: '',
+      results: [],
+    });
+    try {
+      await Voice.start('en-US');
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  componentWillUnmount() {
+    Voice.destroy().then(Voice.removeAllListeners);
+  }
+
+ render() {
+  return (
+    <View>
+      <Text style={styles.transcript}>
+          Transcript
+      </Text>
+      {this.state.results.map((result, index) => <Text style={styles.transcript}> {result}</Text>
+      )}
+      <Button style={styles.transcript}
+      onPress={this._startRecognition.bind(this)}
+      title="Start"></Button>
+    </View>
+  );
+  }
 }
-AppRegistry.registerComponent('VoiceNative', () => VoiceNative);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    marginBottom: 10
+  },
+  transcript: {
+    textAlign: 'center',
+    color: '#B0171F',
+    marginBottom: 1,
+    top: '400%',
+  },
+})
+
+export default App;
+
+//react-native unlink @react-native-voice/voice
